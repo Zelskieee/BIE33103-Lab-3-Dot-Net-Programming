341f6ad6987f279ea3548beed2d0ad01f15ca950
Mohamad Arif Azinuddin Bin Zaidi <chojjaarif2002@gmail.com>
2023-11-05 14:47:32 +0800

    Add project files.

---
+++ MasterPages/ContactUs.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Lab3.MasterPages
+{
+    public partial class ContactUs : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void submitForm_Click(object sender, EventArgs e)
+        {
+            // Handle form submission and clearing logic here
+            // Clear form fields
+            name.Text = string.Empty;
+            email.Text = string.Empty;
+            message.Text = string.Empty;
+        }
+    }
+}
---
+++ MasterPages/Login.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab3.MasterPages
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            lblStatus.Visible = false; // Initially hide the status label.
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string id = TextBox3.Text; // Retrieve the ID entered by the user.
+            string pw = TextBox1.Text; // Retrieve the password entered by the user.
+
+            if (id.Equals("admin") && pw.Equals("admin"))
+            {
+                //Re-direct to another page
+                Response.Redirect("default.aspx");
+            }
+            else
+            {
+                lblStatus.Visible = true; // Make the label visible.
+                lblStatus.Text = "Login unsuccessful!"; // Display a message on the page to indicate login failure.
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            // Clear the text inside ID and Password TextBoxes.
+            TextBox3.Text = string.Empty;
+            TextBox1.Text = string.Empty;
+            lblStatus.Visible = false; // Hide the status label.
+        }
+    }
+}
